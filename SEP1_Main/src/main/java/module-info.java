c488f6578efe4d6950a8b5723672198e59276b56
samuel1212703 <37654743+samuel1212703@users.noreply.github.com>
2021-12-09 17:35:53 +0100

    setup
    Already made script added

---
+++ SEP1_Main/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.sep1_main {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.sep1_main to javafx.fxml;
+    exports com.example.sep1_main;
+}
